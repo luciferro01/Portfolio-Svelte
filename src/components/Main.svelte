171a257b0a6f917184b3443a68fced3db67b6885
luciferro01 <lucifer1111ronaldo@gmail.com>
2023-11-09 22:29:51 +0530

    Adding About Me Section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -100,5 +100,140 @@
       ><i class="fa-regular fa-circle-play" />
       <p>Watch the video</p>
     </a>
+
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+      <Step step={steps[0]}>
+        <p>
+          Smoljames Store is a a merchanising store created with <strong
+            class="text-violet-400"
+            >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong
+          > Commerce.js is a product CMS and Stripe is used for all transaction handling.
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>
+          Ultimate Todos is a Full Stack <strong class="text-violet-400"
+            >Next.js</strong
+          >,
+          <strong class="text-violet-400">Node.js + Express.js</strong>
+          & <strong class="text-violet-400">Firebase</strong> CRUD application that
+          allows a user to login, manage a tidy and efficacious todo list, and persist
+          this information across devices.
+        </p>
+      </Step>
+      <Step step={steps[2]}>
+        <p>
+          The Pokédex is a <strong class="text-violet-400"
+            >SvelteKit & TailwindCSS</strong
+          >
+          web application, hosted on
+          <strong class="text-violet-400">Netlify</strong>, that consumes and
+          caches the
+          <strong class="text-violet-400">Pokémon API</strong>
+          to display all Pokémon information. Gotta catch them all!
+        </p>
+      </Step>
+    </div>
+  </section>
+
+  <!-- About Me Section -->
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-large sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="poppins text-violet-400">about</span> me.
+      </h3>
+    </div>
+    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="sm:text-5xl text-5xl lg:text-6xl text-slate-500 font-semibold"
+          >
+            0{index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+    <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+      The <span class="text-violet-400">Complete</span> Package
+    </h5>
+    <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+      <p>Scroll to see more &rarr;</p>
+    </div>
+    <div
+      class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    >
+      <table class="bg-white text-slate-700 rounded text-center">
+        <thead class={"border-b border-solid border-slate-200  "}>
+          <tr class="">
+            <th />
+            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+            <th class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+              >Me</th
+            >
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Dedication</td
+            >
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Critical Thought</td
+            >
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr>
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Interpersonal Skills</td
+            >
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr class="border-t border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Progamming Ability</td
+            >
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <p class="mx-auto">So why not invest?</p>
   </section>
 </main>
